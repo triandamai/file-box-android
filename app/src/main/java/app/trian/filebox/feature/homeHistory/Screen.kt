03fcbbd6c5b19fa79518ab8f1e8b40c6eed0afe8
trian damai <triannurizkillah@gmail.com>
2022-12-23 18:05:57 +0700

    feat: add send entity

--- app/src/main/java/app/trian/filebox/feature/homeHistory/Screen.kt
+++ app/src/main/java/app/trian/filebox/feature/homeHistory/Screen.kt
@@ -1,10 +1,7 @@
 package app.trian.filebox.feature.homeHistory
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/app/trian/filebox/feature/sendFileOverview/components/BottomSheetListDevice.kt
+++ app/src/main/java/app/trian/filebox/feature/sendFileOverview/components/BottomSheetListDevice.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ExpandLess
 import androidx.compose.material.icons.outlined.ExpandMore
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
